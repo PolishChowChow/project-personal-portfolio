2af959c3f5e8a374d8b54e82a746de8efa44f1bb
PolishChowChow <bmaselko98@gmail.com>
2023-11-01 20:26:08 +0100

    fixed form validation, added icon to notification, fixing some styles

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const asideBarLinks = document.querySelectorAll("a.aside-bar-link");
+const icon = document.querySelector("button#icon");
+const cancelNavigation = document.querySelector("section.cancel-navigation");
+const form = document.querySelector("form");
+const closeNavbarButton = document.querySelector("button#closeNavBar");
+/*giving copyright sign, name and year to the footer paragraph*/
 function setCopyright(){
     const copyrightParagraph = document.querySelector("#copyright");
     const year = new Date().getFullYear();
@@ -18,17 +24,22 @@ function closeNavigation(){
     cancel.classList.toggle("active");
     document.body.style.overflow = "visible";
 }
+
+/*showing communicates*/
 function setMessage(type, content){
     const errorContainer = document.querySelector("section#error");
     const successContainer = document.querySelector("section#success");
+    let icon = null;
     let container = null;
     if(type==="error"){
         container = errorContainer;
+        icon = '<i class="fa-regular fa-circle-xmark"></i>';
     }
     else{
         container = successContainer;
+        icon = '<i class="fa-regular fa-square-check"></i>';
     }
-    container.textContent = content;
+    container.innerHTML = `${content} ${icon}`;
     container.classList.add("active");
     const intervalSetted = setInterval(()=>{
         container.classList.remove("active");
@@ -40,30 +51,39 @@ function setMessage(type, content){
         }
     })
 }
-function validate(){
+/*validating data in input fields*/
+function validate(event){
     const email = document.querySelector("input#email");
     const topic = document.querySelector("input#topic");
     const desc = document.querySelector("textarea#description");
     const emailRegex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/;
     const testResult = emailRegex.test(email.value);
-    console.log(testResult);
     if(email.value==="" || topic.value==="" || desc.value===""){
         setMessage("error","You cannot let fields being empty");
-        return;
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
     }
-    if(!emailRegex.test(email.value)){
+    if(!testResult){
         setMessage("error","Bad email type, it should like something like: test_address@email.com");
-        return;
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
     }
+    event.target.submit();
     setMessage("success","Sending email ended successfully");
+    return true;
 }
-const asideBarLinks = document.querySelectorAll("a.aside-bar-link");
-const icon = document.querySelector("button#icon");
-const cancelNavigation = document.querySelector("section.cancel-navigation");
-const form = document.querySelector("form");
-form.addEventListener("submit",validate)
+
+form.addEventListener("submit",(event)=>{
+    console.log(validate(event));
+    if(!validate(event)){
+        event.preventDefault();
+    }
+})
 cancelNavigation.addEventListener("click",closeNavigation);
 icon.addEventListener("click",openNavigation);
+closeNavbarButton.addEventListener("click",closeNavigation);
 asideBarLinks.forEach(asideBarLink=> {
     asideBarLink.addEventListener("click",closeNavigation)
 });
